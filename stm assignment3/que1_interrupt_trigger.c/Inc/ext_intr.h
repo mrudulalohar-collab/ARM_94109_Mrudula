f30e84ca8658be6f99d6f6a7c5f4176b311491c7
mrudulalohar-collab <mrudulalohar@gmail.com>
2025-12-19 13:12:37 +0530

    stm assignment 3

---
+++ stm assignment3/que1_interrupt_trigger.c/Inc/ext_intr.h
@@ -0,0 +1,15 @@
+/*
+ * ext_intr.h
+ *
+ *  Created on: Dec 19, 2025
+ *      Author: ADMIN
+ */
+
+
+#ifndef EXT_INTR_H_
+#define EXT_INTR_H_
+
+void extint_init(void);
+
+#endif /* EXT_INTR_H_ */
+
