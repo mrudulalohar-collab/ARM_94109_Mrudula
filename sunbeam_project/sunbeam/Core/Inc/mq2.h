eaa6df36f075a47cfaaa68cd72f9aab12167c844
mrudulalohar-collab <mrudulalohar@gmail.com>
2026-01-02 08:47:19 +0530

    sunbeam_project

---
+++ sunbeam_project/sunbeam/Core/Inc/mq2.h
@@ -0,0 +1,16 @@
+
+/*
+ * mq2.h
+ *
+ *  Created on: Dec 31, 2025
+ *      Author: ADMIN
+ */
+
+#ifndef MQ2_H
+#define MQ2_H
+
+#include <stdint.h>
+
+uint32_t mq2_value(uint32_t adc_raw);
+
+#endif
---
+++ sunbeam_project/sunbeam/Core/Src/my_lcd.c
@@ -0,0 +1,8 @@
+/*
+ * my_lcd.c
+ *
+ *  Created on: Jan 2, 2026
+ *      Author: ADMIN
+ */
+
+
